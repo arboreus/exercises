02c5bab2dd8e385722f53a9dcdc0463d96fb2854
Dmitry Salimov <dmitry.salimov@bluebyte.de>
2015-07-29 17:00:13 +0200

    solved 23 and 24

--- project_euler.py
+++ project_euler.py
@@ -493,9 +493,47 @@ sum(names_scores)
 #A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
 #As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
 #Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+import itertools
 
+def div(n):
+    if n == 1: return [1]
+    return [x for x in range(1, n) if n%x==0]
 
+nums_all = [(x, div(x)) for x in range(1, 28124)]
+nums_abundant = [x for x, y in nums_all if sum(y) > x]
+nums_sum2 = list(set([sum(x) for x in itertools.combinations(nums_abundant, 2) if sum(x) < 28124] + [x*2 for x in nums_abundant]))
+nums_result = [x for x in range(1, 28124) if x not in nums_sum2]
+sum(nums_result)
 
+#%% 24) Lexicographic permutations
+#A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+#012   021   102   120   201   210
+#What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+import math
+
+def sub(n, i):
+    if n > 0 and i > 0:
+        c = 0
+        while n > math.factorial(i):
+            n -= math.factorial(i)
+            c += 1
+        return [(n, c*math.factorial(i), c)] + sub(n, i-1)
+    else:
+        return [(n, 0, 0)]
+        
+pos = [z for x, y, z in sub(10**6, 9)]
+
+def shift(l):
+    string = list(range(10))
+    result = []
+    for i in l:
+        result.append(string[i])
+        string.pop(i)
+    return "".join([str(x) for x in result])
+        
+shift(pos)
+    
 #%%
+
 
 
